821aa26be6fc538a4e3b9f93931fe4f4c3633bb9
antonyadhiban <gtadhib@gmail.com>
2021-05-26 23:00:57 +0530

    Init App

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+const CracoEsbuildPlugin = require('craco-esbuild');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoEsbuildPlugin,
+    },
+  ],
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+};
